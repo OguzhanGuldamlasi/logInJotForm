18a3af5504d9d3acd416a0cc165265afd4dc0178
OguzhanGuldamlasi <oguldamlasi@etu.edu.tr>
2019-10-01 11:00:22 +0300

    Added validations

--- loginflow/src/components/LoginPage.js
+++ loginflow/src/components/LoginPage.js
@@ -1,27 +1,48 @@
 import React from 'react';
-import {BrowserRouter,Route} from "react-router-dom";
 
 class LoginPage extends React.Component {
     constructor(props){
         super(props);
         this.state={
-             username:'',
-             password:''
+             username:null,
+             password:null,
+             userNameError:false,
+             passwordError:false,
+             Error:false
         };
-    }
+    };
     async submitUser(){
         let user={
             username:this.state.username,
             password:this.state.password
         };
-         await window.JF.userLogin(user, ()=>window.JF.getForms(function (response) {
-             for (let i = 0; i < response.length; i++) {
-                 document.writeln(response[i])
-             }
-         }),(err)=>console.log(err));
+         if(user.username===''||user.username===null){
+             document.getElementsByClassName("UserName")[0].style.backgroundColor="red";
+             this.setState({userNameError:true})
+            return ;
+         }
+         else{
+             document.getElementsByClassName("UserName")[0].style.backgroundColor="white";
+             this.setState({userNameError:false})
+         }
+         if(user.password===''||user.password===null){
+             document.getElementsByClassName("password")[0].style.backgroundColor="red";
+             this.setState({passwordError:true})
+            return ;
+         }
+         else{
+             document.getElementsByClassName("password")[0].style.backgroundColor="white";
+             this.setState({passwordError:false})
+         }
+        await window.JF.userLogin(user, ()=>window.JF.getForms(function (response) {
+            for (let i = 0; i < response.length; i++) {
+                document.writeln(response[i].title)
+            }
+        }),(err)=>this.setState({Error:true}));
+
     }
     onChangeUserName = (e) => {
-        this.setState({
+         this.setState({
             username: e.target.value,
         });
     };
@@ -32,12 +53,18 @@ class LoginPage extends React.Component {
     };
     render() {
         return (
-
             <div>
+                <div>
                 <input className="UserName" placeholder="UserName"  onChange={e => this.onChangeUserName(e)}/>
-                <input className="password" placeholder="password" type="password" onChange={e => this.onChangePassword(e)} />
-                <button className="submitButton" onClick={()=> this.submitUser()}>Login</button>
+                <span  id="errorMsg1" className="userNameErrorMsg" style={{visibility: this.state.userNameError ? 'visible' : 'hidden' }} >Bu alan boş bırakılamaz</span>
                 </div>
+                <div>
+                <input className="password" placeholder="password" type="password" onChange={e => this.onChangePassword(e)} />
+                <span className="passwordErrorMsg" style={{visibility: this.state.passwordError ? 'visible' : 'hidden' }} >Bu alan boş bırakılamaz</span>
+                </div>
+                <button className="submitButton" onClick={()=> this.submitUser()}>Login</button>
+                <span className="Error"style={{visibility: this.state.Error ? 'visible' : 'hidden' }} >hatalı girdi</span>
+            </div>
         )
     }
 }
